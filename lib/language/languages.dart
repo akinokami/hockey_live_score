8ae9e8b54daadc0a59eeb5c35b23790d2432ad09
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-09-12 10:58:56 +0800

    a

---
+++ lib/language/languages.dart
@@ -0,0 +1,14 @@
+import 'package:hockey_live_score/language/vi_language.dart';
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+import 'en_language.dart';
+import 'hindi_language.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        //'zh_CN': ZhLanguage().zhLanguage,
+        'en_US': EnLanguage().enLanguage,
+        'hi_IN': HiLanguage().hiLanguage,
+        'vi_VN': ViLanguage().viLanguage,
+      };
+}
